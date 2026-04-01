ccf4ebc6111784b21c15d8b3b00afca9e92e0b94
Amaranto <danielmares2003@gmail.com>
2026-04-01 12:06:09 -0400

    IR Generation for Aritmetics Exp

--- hulk/src/main.rs
+++ hulk/src/main.rs
@@ -1,13 +1,24 @@
-use lalrpop_util::lalrpop_mod; 
+use lalrpop_util::lalrpop_mod;
 
 pub mod ast;
 pub mod expr_visitor;
-lalrpop_mod!(grammar); 
-
+lalrpop_mod!(grammar);
+pub mod codegen;
+pub mod codegen_visitor;
 fn main() {
-
     let expr = grammar::ExprParser::new();
-    
-    let resultado = expr.parse("(2 + 2)* 4").unwrap();
-    println!("El resultado es: {}", format!("{:?}", resultado));
+
+    let input = "(2 + 2)* 4 > (5+5)";
+
+    match expr.parse(input) {
+        Ok(ast) => {
+            println!("AST generado: {:?}\n", ast);
+
+            match codegen::generate_ir_and_execute(&ast, "hulk_module", Some("output.ll")) {
+                Ok(result) => println!("Resultado de ejecución JIT: {}", result),
+                Err(e) => eprintln!("Error durante codegen/JIT: {}", e),
+            }
+        }
+        Err(e) => eprintln!("Error al parsear: {}", e),
+    }
 }
