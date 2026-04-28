b30e241a4762ad66358c4e56cbeb43d2c003336b
Amaranto <danielmares2003@gmail.com>
2026-04-28 13:02:20 -0400

    Codegen for blocks

--- hulk/src/codegen.rs
+++ hulk/src/codegen.rs
@@ -6,7 +6,9 @@ use inkwell::targets::{InitializationConfig, Target};
 use std::fs;
 use std::io;
 
-use crate::nodes::typedexpr_node::TypedExpr;
+use crate::nodes::block_node::BlockNode;
+use crate::nodes::program_node::{Program, Statement};
+use crate::nodes::typedexpr_node::{Expr, TypedExpr};
 
 
 
@@ -102,26 +104,80 @@ impl<'ctx> CodeGenerator<'ctx> {
     }
 }
 
-pub fn generate_ir_and_execute(
-    ast: &TypedExpr,
+// pub fn generate_ir_and_execute(
+//     ast: &TypedExpr,
+//     module_name: &str,
+//     ir_output: Option<&str>,
+// ) -> Result<i32, Box<dyn std::error::Error>> {
+//     Target::initialize_native(&InitializationConfig::default())
+//         .map_err(|e| Box::<dyn std::error::Error>::from(io::Error::new(io::ErrorKind::Other, e)))?;
+
+//     let context = Context::create();
+//     let mut code_gen = CodeGenerator::new(&context, module_name);
+
+//     let i32_type = context.i32_type();
+//     let fn_type = i32_type.fn_type(&[], false);
+//     let function = code_gen.module.add_function("main", fn_type, None);
+//     let basic_block = context.append_basic_block(function, "entry");
+//     code_gen.builder.position_at_end(basic_block);
+
+//     let result = ast.accept(&mut code_gen);
+//     code_gen.builder.build_return(Some(&result))?;
+
+//     if let Some(path) = ir_output {
+//         code_gen.save_ir(path)?;
+//     }
+
+//     code_gen.compile_and_execute()
+// }
+
+pub fn compile_hulk_program(
+    program: Program,
     module_name: &str,
     ir_output: Option<&str>,
 ) -> Result<i32, Box<dyn std::error::Error>> {
+    // 1. Inicializar LLVM
     Target::initialize_native(&InitializationConfig::default())
         .map_err(|e| Box::<dyn std::error::Error>::from(io::Error::new(io::ErrorKind::Other, e)))?;
 
     let context = Context::create();
     let mut code_gen = CodeGenerator::new(&context, module_name);
 
+    // 2. Preparar el punto de entrada 'main'
     let i32_type = context.i32_type();
     let fn_type = i32_type.fn_type(&[], false);
     let function = code_gen.module.add_function("main", fn_type, None);
     let basic_block = context.append_basic_block(function, "entry");
     code_gen.builder.position_at_end(basic_block);
 
-    let result = ast.accept(&mut code_gen);
-    code_gen.builder.build_return(Some(&result))?;
+    // 3. Convertir el programa en un bloque ejecutable
+    // Separamos declaraciones de funciones de las expresiones top-level
+    let mut expressions = Vec::new();
+    for stmt in program.statements {
+        match stmt {
+            Statement::Expression(expr) => expressions.push(expr),
+            Statement::FunctionDecl(_decl) => {
+                // TODO: Implementar registro de funciones globales en el módulo
+            }
+        }
+    }
 
+    // Envolvemos todo en un bloque para reusar visit_block y obtener el último valor
+    let top_level_block = TypedExpr::new(Expr::Block(BlockNode::new(expressions)));
+    
+    // 4. Generar el IR
+    let result = top_level_block.accept(&mut code_gen);
+    
+    // Aseguramos que el retorno sea i32 (booleano o número en tu sistema actual)
+    let return_val = if result.is_int_value() {
+        result.into_int_value()
+    } else {
+        context.i32_type().const_int(0, false)
+    };
+
+    code_gen.builder.build_return(Some(&return_val)).unwrap();
+
+    // 5. Opcionales y ejecución
     if let Some(path) = ir_output {
         code_gen.save_ir(path)?;
     }
