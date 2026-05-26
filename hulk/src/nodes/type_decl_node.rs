0d48687630728214a92f045e25127034f40431dc
Amaranto <danielmares2003@gmail.com>
2026-05-26 16:29:22 -0400

    Add inheritage support

--- hulk/src/nodes/type_decl_node.rs
+++ hulk/src/nodes/type_decl_node.rs
@@ -14,11 +14,24 @@ impl AttributeNode {
 }
 
 #[derive(Debug)]
+pub struct InheritanceClause {
+    pub parent_name: LiteralNode,
+    pub parent_args: Option<Vec<TypedExpr>>, // None = heredar argumentos implícitamente
+}
+
+impl InheritanceClause {
+    pub fn new(parent_name: LiteralNode, parent_args: Option<Vec<TypedExpr>>) -> Self {
+        Self { parent_name, parent_args }
+    }
+}
+
+#[derive(Debug)]
 pub struct TypeDeclNode {
     pub name: LiteralNode,
     pub params: Vec<(LiteralNode, HulkType)>,
     pub attributes: Vec<AttributeNode>,
     pub methods: Vec<FunctionDecl>,
+    pub inheritance: Option<InheritanceClause>,
 }
 
 pub enum TypeBodyItem {
@@ -28,6 +41,10 @@ pub enum TypeBodyItem {
 
 impl TypeDeclNode {
     pub fn new(name: LiteralNode, params: Vec<(LiteralNode, HulkType)>, attributes: Vec<AttributeNode>, methods: Vec<FunctionDecl>) -> Self {
-        Self { name, params, attributes, methods }
+        Self { name, params, attributes, methods, inheritance: None }
+    }
+
+    pub fn with_inheritance(name: LiteralNode, params: Vec<(LiteralNode, HulkType)>,attributes: Vec<AttributeNode>,methods: Vec<FunctionDecl>,inheritance: Option<InheritanceClause>) -> Self {
+        Self { name, params,attributes,methods,inheritance }
     }
 }
